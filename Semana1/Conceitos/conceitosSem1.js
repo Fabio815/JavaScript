e64dd107b2cf39afadb2425a05b011ed70fa0844
Fábio <fabiomotamarques815@gmail.com>
2024-11-08 08:09:57 -0300

    Alteracao

--- Semana1/Conceitos/conceitosSem1.js
+++ Semana1/Conceitos/conceitosSem1.js
@@ -1,11 +1,4 @@
+const idade = 18;
 
-let num = 9;
-var num1 = 5;
-
-function soma(numero){
-    num += num1;
-}
-
-soma(num1);
-
-console.log(num);
+idade = 10;
+console.log(idade);
--- Semana1/RepresentPessoa.js
+++ Semana1/RepresentPessoa.js
@@ -1,6 +1,15 @@
 const pessoa = {
     nome: "Joelson",
     idade: 18,
-    cidade: "Curitiba"
-    
-};
+    cidade: "Curitiba",
+
+    aniversario(){
+        this.idade += 1;
+    },
+    imprimirDados(){
+        console.log(`Nome: ${this.nome}\nIdade: ${this.idade}\nCidade ${this.cidade}`);
+    }
+}
+
+pessoa.aniversario();
+pessoa.imprimirDados();

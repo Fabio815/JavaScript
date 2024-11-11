a42ccda55a563dee182407e6c0d02f1c90590793
Fabio815 <fabiomotamarques815@gmail.com>
2024-11-11 19:41:26 -0300

    Feito e testado

--- Semana1/RepresentPessoa.js
+++ Semana1/RepresentPessoa.js
@@ -6,14 +6,30 @@ const pessoas  = [
     }
 ];
 
-pessoas.push({nome: "Fábio", idade: 24, cidade: "Gaspar"});
-pessoas.push({nome: "Larissa", idade: 24, cidade: "Gaspar"});
+//pessoas.push({nome: "Fábio", idade: 24, cidade: "Gaspar"});
+//pessoas.push({nome: "Larissa", idade: 24, cidade: "Gaspar"});
 
 //Criar uma função que devolve uma pessoa recevbendo nome, idade, cidade. Adicionar a pessoa na lista e imprimir a pessoas.
-pessoas.push({nome: "Jean", idade: 30, cidade: "Blumenau"});
-pessoas.push({nome: "Luz", idade: 21, cidade: "Belem"});
-pessoas.push({nome: "Erico", idade: 30, cidade: "Gaspar"});
+//pessoas.push({nome: "Jean", idade: 30, cidade: "Blumenau"});
+//pessoas.push({nome: "Luz", idade: 21, cidade: "Belem"});
+//pessoas.push({nome: "Erico", idade: 30, cidade: "Gaspar"});
 
-for(let i = 0; i < pessoas.length; i++){
-    console.log(pessoas[i]);
+function adicionarPessoas(nomePessoa, idadePessoa, cidadePessoa){
+    pessoas.push({nome: nomePessoa, idade: idadePessoa, cidade: cidadePessoa});
 }
+
+function imprimirPessoas(nomeProcurado){
+    for(let i = 0; i < pessoas.length; i++){
+        if(pessoas[i].nome === nomeProcurado){
+            console.log(pessoas[i]);
+        }
+    }
+}
+
+adicionarPessoas("Fábio", 24, "Curitiba");
+adicionarPessoas("Larissa", 24, "Gaspar");
+adicionarPessoas("Jean", 30, "Blumenau");
+adicionarPessoas("Luz", 21, "Belem");
+adicionarPessoas("Erico", 30, "Gaspar");
+
+imprimirPessoas("jean");

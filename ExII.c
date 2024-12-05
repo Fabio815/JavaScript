2a54cb85458792cc5f421656ba834fdb8434e1b7
Fábio <fabiomotamarques815@gmail.com>
2024-12-05 18:19:56 -0300

    realloc

--- ExII.c
+++ ExII.c
@@ -44,6 +44,53 @@ int main()
         printf("%i\n", vetorNum.listaDeNumeros[i].valor);
     }
 
+
     free(vetorNum.listaDeNumeros);
     return 0;
 }
+
+/* #include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr;
+    int n1 = 5, n2 = 10;
+
+    // Alocando memória inicial para 5 inteiros
+    ptr = (int*) malloc(n1 * sizeof(int));
+
+    // Verificando se a memória foi alocada com sucesso
+    if (ptr == NULL) {
+        printf("Erro na alocação de memória.\n");
+        return 1;
+    }
+
+    // Inicializando a memória alocada
+    for (int i = 0; i < n1; i++) {
+        ptr[i] = i + 1;
+    }
+
+    // Realocando memória para 10 inteiros
+    ptr = (int*) realloc(ptr, n2 * sizeof(int));
+
+    // Verificando se a realocação foi bem-sucedida
+    if (ptr == NULL) {
+        printf("Erro na realocação de memória.\n");
+        return 1;
+    }
+
+    // Inicializando a nova memória alocada
+    for (int i = n1; i < n2; i++) {
+        ptr[i] = i + 1;
+    }
+
+    // Imprimindo os valores armazenados
+    for (int i = 0; i < n2; i++) {
+        printf("%d ", ptr[i]);
+    }
+
+    // Liberando a memória alocada
+    free(ptr);
+
+    return 0;
+}*/
